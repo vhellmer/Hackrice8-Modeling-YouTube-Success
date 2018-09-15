3d4302800899504210fda02c63e9fa17eea61fc2
Valerie Hellmer <valerie.hellmer@gmail.com>
2018-09-15 10:32:50 -0500

    updated comments

--- youtube-new/youtubedata.py
+++ youtube-new/youtubedata.py
@@ -8,7 +8,16 @@ import csv
 #row 5 = detailed date posted
 #row 6 = tags
 #row 7 = views
+#row 8 = likes
+#row 9 = dislikes
+#row 10 = comment count
+#row 11 = thumbnail link
+#row 12, 13, 14 = booleans comments_disabled, ratings_disabled, video_error_or_removed
+#row 15 = description
+
 with open('USvideos.csv', encoding="utf-8") as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
+    #nums = [''.join([c for c in i if c.isdigit()]) for i in data] #extract only the numbers from lines
+    #sortedlist = sorted(readCSV, key=lambda row: float(row[7]), reverse=True)
     for row in readCSV:
-        print(row[8])
+    	print(row[7])
