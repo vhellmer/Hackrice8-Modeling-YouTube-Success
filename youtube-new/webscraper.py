e1e672e9471fb21be353fb08446c55217ed0ae94
Valerie Hellmer <valerie.hellmer@gmail.com>
2018-09-15 13:27:06 -0500

    Basic functionality of gathering views, likes, comment_count

--- youtube-new/webscraper.py
+++ youtube-new/webscraper.py
@@ -22,9 +22,31 @@ driver = webdriver.Chrome()
 
 # File for strains to be written to
 # new_file = open("current_video_data.csv", "w")
-
+url_list = url_list[1:]
 for url in url_list:
     driver.get("https://www.youtube.com/watch?v=" + url)
-    sleep(5)
+    sleep(2)
+    try:
+        element_present = EC.presence_of_element_located((By.XPATH, '//*[@id="count"]/yt-view-count-renderer/span[1]'))
+        wait(driver, 5).until(element_present)
+        views = driver.find_element_by_xpath('//*[@id="count"]/yt-view-count-renderer/span[1]').text
+        print(views)
+    except:
+       print("views failed: ", url)
+
+    sleep(3)
+    likes = driver.find_element_by_xpath('//*[@id="top-level-buttons"]/ytd-toggle-button-renderer[1]/a').text
+    print(likes)
+    #print("likes failed: ", url)
+    #try:
+        #dislikes = driver.find_element_by_xpath('//*[@id="text"]').text
+        #print(dislikes)
+    #except:
+        #print("dislikes failed: ", url)
+    try:
+        comments = driver.find_element_by_xpath('//*[@id="count"]/yt-formatted-string').text
+        print(comments)
+    except:
+        print("comments failed: ", url)
 
 driver.close()
