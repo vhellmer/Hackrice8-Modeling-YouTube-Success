78eb47023b3ba1f3ac4e2acdd9317d6948e82287
kenzeng24 <kgz1@rice.edu>
2018-09-15 23:30:05 -0500

    renamed variables and adding some graphing functions

---
+++ youtube-new/Optimization.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.formula.api as sm
+#INPUT ken2
+
+def subsets(input):
+    if not input:
+        return [[]]
+    else:
+        children = subsets(input[1:])
+        childrenPlus = [[input[0]] + x for x in children]
+        return children + childrenPlus
+
+
+ken2 = pd.read_csv("combine5.csv")
+column_list = list(ken2.columns)
+# for columns in column_list:
+#     ken2[columns] = ken2[columns].apply(np.log1p)
+
+
+column_list = column_list[1:]
+head_power_set = subsets(column_list)
+
+head_power_set = head_power_set[1:]
+
+maxr = 0
+maxcombo = None
+count = 1.0
+total = len(head_power_set)
+for combos in head_power_set:
+    print(count / total)
+    count += 1
+    formula_string = "views_change ~ "
+    if combos:
+        for header_index in range(len(combos)):
+            if header_index == 0:
+                formula_string += combos[0]
+            else:
+                formula_string = formula_string + " + " + combos[header_index]
+        # print(formula_string)
+        result = sm.ols(formula= formula_string, data=ken2).fit()
+
+        if result.rsquared > maxr:
+            maxr = result.rsquared
+            maxcombo = result
+
+print(maxr)
+print(maxcombo.summary())
--- youtube-new/generate_csv2.py
+++ youtube-new/generate_csv2.py
@@ -1,5 +1,56 @@
 import csv
 
+
+def stringtoint(stringdate):
+    first = stringdate.split("T")[0]
+    second = first.split("-")
+    year = int(second[0])
+    month = int(second[1])
+    day = int(second[2])
+    return year, month, day
+
+
+def monthday_array():
+    """
+    helper function for days_uploaded
+    """
+    monthday = [31, 0]
+    for i in range(7, 0, -1):
+        if i % 2 == 1:
+            monthday.insert(0, monthday[0] + 31)
+        elif i == 2:
+            monthday.insert(0, monthday[0] + 28)
+        else:
+            monthday.insert(0, monthday[0] + 30)
+    for i in range(9, 12):
+        if i == 11:
+            monthday.append(monthday[-1] - 31)
+        else:
+            monthday.append(monthday[-1] - 30)
+    monthday.insert(0, 0)
+    return monthday
+
+
+monthday = monthday_array()
+#print (monthday)
+
+
+def days_uploaded(year, month, day):
+    # assuming each year has 365 days
+    days = (2018 - year) * 365
+
+    days += monthday[month]
+
+    days += 15 - day
+
+    return days
+
+
+x,y,z = stringtoint("2017-01-13T17:13:01.000Z")
+#print(x,y,z)
+#print(days_uploaded(x, y, z))
+#print(days_uploaded(2017, 1, 13)), "expected: 610"
+
 def new_cvs(original_stats, new_stats):
 
     new_data = csv.reader(open(new_stats))
@@ -35,7 +86,9 @@ def new_cvs(original_stats, new_stats):
         if url in url_dict:
 
             # publish time, views, likes, dislikes, comment_count
-            publishing_time = url_dict[url][4]
+            x,y,z = stringtoint(url_dict[url][4])
+            publishing_time = days_uploaded(x, y, z)
+
             views = float(url_dict[url][6])
             likes = float(url_dict[url][7])
             dislikes = float(url_dict[url][8])
@@ -53,7 +106,7 @@ def new_cvs(original_stats, new_stats):
                 vid_dict[url] = [publishing_time, views, likes, dislikes, comment_count, new_views, new_likes, new_dislikes, new_comment_count, views_change, likes_change, dislikes_change, comment_count_change]
 
     # create the csv file
-    csv_file = open('combined2.csv', 'w')
+    csv_file = open('combined4.csv', 'w')
     writer = csv.writer(csv_file)
     writer.writerow(["video_id", "publish_time","views", "likes", "dislikes", "comment_count","new_views", "new likes", "new dislikes", "new comment_count", "views_change", "likes_change", "dislikes_change", "comment_count_change"])
     for key, value in vid_dict.items():
