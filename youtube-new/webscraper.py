6011deb2e204f10b5cdb985e088897d336f6f7a9
Valerie Hellmer <valerie.hellmer@gmail.com>
2018-09-15 20:52:02 -0500

    Fixed percent change files, vid_data up to 2715 (i->4000)

--- youtube-new/webscraper.py
+++ youtube-new/webscraper.py
@@ -21,7 +21,7 @@ driver = webdriver.Chrome()
 vid_dict = defaultdict(list)
 
 # Goes through a section of urls in list
-for i in range(3000, 3500):
+for i in range(3500, 4000):
 
     url = url_list[i]
     vid_dict[url] = []
